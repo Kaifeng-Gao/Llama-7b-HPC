dccf3e06ea1818024a17c11faf699a53573a8c88
YixiaoChen <yixiaotommychen@outlook.com>
2024-04-04 18:48:23 -0400

    Update Templating

--- dataset_converter.py
+++ dataset_converter.py
@@ -1,11 +1,62 @@
-def format(sample):
-    context = f"<s>[INST] Here's the dataset overview: {sample['context']}." if len(sample['context']) > 0 else None
-    instruction = f" Provide SQL that answer: {sample['question']}"
-    reponse = f" [/INST] {sample['answer']}"
-    # join all parts together
-    prompt = "".join([i for i in [context, instruction, reponse] if i is not None])
-    return prompt
+import random
 
+# Define multiple templates for formatting the sample
+def template_a(sample):
+    prefix = "<s>[INST]"
+    if sample['context']:
+        instruction = f"With the given context '{sample['context']}', how would you write SQL to retrieve data: {sample['question']}"
+    else:
+        instruction = f"How would you write SQL to answer: {sample['question']}"
+    return f"{prefix} {instruction} [/INST] {sample['answer']}"
+
+def template_b(sample):
+    prefix = "<s>[INST]"
+    if sample['context']:
+        instruction = f"How can the question '{sample['question']}' be answered using SQL, considering the context: '{sample['context']}'"
+    else:
+        instruction = f"How can the question '{sample['question']}' be answered using SQL"
+    return f"{prefix} {instruction} [/INST] {sample['answer']}"
+
+def template_c(sample):
+    prefix = "<s>[INST]"
+    if sample['context']:
+        instruction = f"Dataset overview: '{sample['context']}'. Please demonstrate how SQL can be used to address the question: '{sample['question']}'"
+    else:
+        instruction = f"Please demonstrate how SQL can be used to address the question: '{sample['question']}'."
+    return f"{prefix} {instruction} [/INST] {sample['answer']}"
+
+def template_d(sample):
+    prefix = "<s>[INST]"
+    if sample['context']:
+        instruction = f"Let's collaborate on writing an SQL query to solve for '{sample['question']}' within this context: '{sample['context']}'. How should we approach it"
+    else:
+        instruction = f"Let's collaborate on writing an SQL query to solve for '{sample['question']}'. How should we approach it?"
+    return f"{prefix} {instruction} [/INST] {sample['answer']}"
+
+def template_e(sample):
+    prefix = "<s>[INST]"
+    if sample['context']:
+        instruction = f"As part of our data analysis with the background '{sample['context']}', could you formulate an SQL query to address the inquiry: '{sample['question']}'"
+    else:
+        instruction = f"Could you formulate an SQL query to address the inquiry: '{sample['question']}'"
+    return f"{prefix} {instruction} [/INST] {sample['answer']}"
+
+def template_e(sample):
+    prefix = "<s>[INST]"
+    if sample['context']:
+        instruction = f"You've been tasked with finding information related to :'{sample['question']}', under this context: '{sample['context']}'"
+    else:
+        instruction = f"You've been tasked with finding information related to :'{sample['question']}'"
+    return f"{prefix} {instruction} [/INST] {sample['answer']}"
+
+
+# Function to randomly select a template and format the sample
+def format_sample_randomly(sample):
+    templates = [template_a, template_b, template_c, template_d, template_e]
+    chosen_template = random.choice(templates)
+    return chosen_template(sample)
+
+# Modified template_dataset function that uses the new random format function
 def template_dataset(sample, tokenizer):
-    sample['text'] = f"{format(sample)}{tokenizer.eos_token}"
+    sample['text'] = f"{format_sample_randomly(sample)}{tokenizer.eos_token}"
     return sample
