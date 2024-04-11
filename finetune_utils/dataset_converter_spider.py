fc78c6c639a42f09b0ff9ad66efb56f21aa4c2d6
Kaifeng Gao <kaifeng.gao@outlook.com>
2024-04-10 20:01:11 -0400

    add support for dataset schema in template

--- finetune_utils/dataset_converter_spider.py
+++ finetune_utils/dataset_converter_spider.py
@@ -21,7 +21,21 @@ TEMPLATES = [
     "[INST] How do you formulate an SQL query to address the problem described here: '{question}'? [/INST] \n```sql\n{query}\n```",
     "[INST] Given the database task, '{question}', construct the appropriate SQL command.[/INST] \n```sql\n{query}\n```",
     "[INST] Construct a SQL script to fetch data as per the question: {question} [/INST] \n```sql\n{query}\n```",
-    "[INST] For the challenge outlined in '{question}', what SQL operation would you perform? [/INST] \n```sql\n{query}\n```"
+    "[INST] {question} Given the schema:\n```sql\n{schema}\n```\nWhat SQL query would you write to address this? [/INST] \n```sql\n{query}\n```",
+    "[INST] Examine the SQL query:\n```sql\n{query}\n```\nWhat question does this query aim to answer, assuming the following database schema? \n```sql\n{schema}\n``` [/INST] The query solves the question: {question}",
+    "[INST] To tackle the problem '{question}', formulate an appropriate SQL query using the provided schema:\n```sql\n{schema}\n``` [/INST] \n```sql\n{query}\n```",
+    "[INST] Given the SQL query:\n```sql\n{query}\n```\nAnd the database schema:\n```sql\n{schema}\n```\nInfer the question this query is designed to answer. [/INST] The question is: {question}",
+    "[INST] Suppose you have a database with the following schema:\n```sql\n{schema}\n```\nHow would you retrieve the data needed to answer: {question} [/INST] \n```sql\n{query}\n```",
+    "[INST] Consider the question: {question}\nUsing the schema provided:\n```sql\n{schema}\n```\nConstruct an SQL query to resolve this question. [/INST] \n```sql\n{query}\n```",
+    "[INST] Analyze the given SQL query:\n```sql\n{query}\n```\nBased on the database schema:\n```sql\n{schema}\n```\nWhat question is this query intended to address? [/INST] The query answers the question: {question}",
+    "[INST] Given the following database structure:\n```sql\n{schema}\n```\nHow would you write an SQL statement to find the information needed to resolve this inquiry: {question} [/INST] \n```sql\n{query}\n```",
+    "[INST] {question} Considering the tables and relationships defined below:\n```sql\n{schema}\n```\nWhat SQL command would you execute to handle this request? [/INST] \n```sql\n{query}\n```",
+    "[INST] Examine the provided SQL code:\n```sql\n{query}\n```\nBased on the database layout:\n```sql\n{schema}\n```\nWhat problem does this SQL statement aim to solve? [/INST] The SQL code addresses the following issue: {question}",
+    "[INST] To resolve '{question}', create a suitable SQL query using the given database blueprint:\n```sql\n{schema}\n``` [/INST] \n```sql\n{query}\n```", 
+    "[INST] Analyze the SQL script:\n```sql\n{query}\n```\nAnd the database design:\n```sql\n{schema}\n```\nDeduce the challenge this script is designed to tackle. [/INST] The challenge is: {question}",
+    "[INST] Imagine a database organized as follows:\n```sql\n{schema}\n```\nHow would you fetch the necessary data to respond to: {question} [/INST] \n```sql\n{query}\n```",
+    "[INST] Ponder the dilemma: {question}\nUsing the database structure provided:\n```sql\n{schema}\n```\nDevise an SQL query to address this dilemma. [/INST] \n```sql\n{query}\n```",
+    "[INST] Study the presented SQL command:\n```sql\n{query}\n```\nConsidering the database organization:\n```sql\n{schema}\n```\nWhat issue is this command intended to resolve? [/INST] The command solves the following problem: {question}"
 ]
 
 
@@ -29,11 +43,13 @@ def format_sample_with_template(sample):
     selected_template = random.choice(TEMPLATES)
     question = sample.get('question', '')
     raw_query = sample.get('query', '')
+    schema_list = sample.get('schema', [])
+    schema = "\n".join(schema_list)
     query = sqlparse.format(raw_query, reindent=True, keyword_case='upper')
 
     # Using exception handling to ignore placeholders if data is missing in the sample
     try:
-        formatted_text = selected_template.format(question=question, query=query)
+        formatted_text = selected_template.format(question=question, query=query, schema=schema)
     except KeyError as e:
         # If some keys are missing, makeup or ignore and reformat
         formatted_text = selected_template.format(question=question, query=query)
